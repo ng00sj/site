cd839fd102f88e830a21f27c5939478247d3859d
ng.0_0.sj <123550365+ng00sj@users.noreply.github.com>
2026-08-11 22:03:33 +0800

    Refactor button event listeners for rotation functions

--- star/js/b.js
+++ star/js/b.js
@@ -1,5 +1,15 @@
-const btn = document.getElementById('butRotX');
+const el_butRotX = document.getElementById('butRotX');
+const el_butRotY = document.getElementById('butRotX');
+const el_butRotZ = document.getElementById('butRotX');
 
-btn.addEventListener('click', () => {
+el_butRotX.addEventListener('click', () => {
   rotX();
+});
+
+el_butRotY.addEventListener('click', () => {
+  rotY();
+});
+
+el_butRotZ.addEventListener('click', () => {
+  rotZ();
 });
