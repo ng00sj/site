adce47a4687baeaee2fc0499857a71fa960b9772
ng.0_0.sj <123550365+ng00sj@users.noreply.github.com>
2026-08-11 20:54:00 +0800

    Add redraw function and update rotation handling
    Refactor rotation logic to include redraw function.

--- star/js/0.js
+++ star/js/0.js
@@ -1,11 +1,22 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let total = [1,0,0,0];
+let qTotal = [1,0,0,0];
+let vPoint = [20,0,0,0];
 
 const qStepX = qMake(Math.PI/10,[1,0,0]);
 const qStepY = qMake(Math.PI/10,[0,1,0]);
 
-function rotX() {
+function redraw(rX, rY) {
   ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.arc(canvas.width/2+rX, canvas.height/2+rY,0,2*Math.PI);
+}
+
+function rotX() {
+  qTotal = qMult(qStepX, qTotal);
+  const v = vRot(qTotal, vPoint);
+  const rX = v[0];
+  const rY = v[1];
+  
+  redraw(rX, rY);
 }
