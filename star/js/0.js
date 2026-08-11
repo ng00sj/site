adcc87efef59e05d5ced7c1f1aba9519125b8d30
ng.0_0.sj <123550365+ng00sj@users.noreply.github.com>
2026-08-11 22:00:41 +0800

    Refactor canvas drawing and resizing functions
    Refactored drawing and resizing functions for clarity and organization.

--- star/js/0.js
+++ star/js/0.js
@@ -6,19 +6,42 @@ let vPoint = [20,0,0,0];
 
 const qStepX = qMake(Math.PI/10,[1,0,0]);
 const qStepY = qMake(Math.PI/10,[0,1,0]);
+const qStepY = qMake(Math.PI/10,[0,0,1]);
 
-function redraw(rX, rY) {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function draw(rX, rY) {
+  ctx.beginPath();
   ctx.arc(canvas.width/2+rX, canvas.height/2+rY,1,0,2*Math.PI);
   ctx.fillStyle = "white";
   ctx.fill();
 }
 
-function rotX() {
-  qTotal = qMult(qStepX, qTotal);
+function redraw() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
   const v = vRot(qTotal, vPoint);
   const rX = v[0];
   const rY = v[1];
-  
-  redraw(rX, rY);
+  draw(rX, rY);
 }
+
+function rotX() {
+  qTotal = qMult(qStepX, qTotal);
+  redraw();
+}
+
+function rotY() {
+  qTotal = qMult(qStepY, qTotal);
+  redraw();
+}
+
+function rotZ() {
+  qTotal = qMult(qStepZ, qTotal);
+  redraw();
+}
+
+resizeCanvas();
+redraw(rX, rY);
