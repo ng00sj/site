bcacf45c004a438803c8f5bd2c05441b2e01dee2
ng.0_0.sj <123550365+ng00sj@users.noreply.github.com>
2026-08-11 20:39:35 +0800

    Initialize canvas and define rotation function
    Added canvas setup and rotation function.

--- star/js/0.js
+++ star/js/0.js
@@ -1,1 +1,11 @@
-let total = [1,0,0,0]
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let total = [1,0,0,0];
+
+const qStepX = qMake(Math.PI/10,[1,0,0]);
+const qStepY = qMake(Math.PI/10,[0,1,0]);
+
+function rotX() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+}
