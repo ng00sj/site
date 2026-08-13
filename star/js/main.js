febccf0beb90b0867261172fb8dd42ac3a0b61fc
ng.0_0.sj <123550365+ng00sj@users.noreply.github.com>
2026-08-13 22:55:00 +0800

    Add 3D rotation visualization to main.js

---
+++ star/js/main.js
@@ -0,0 +1,75 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let radius = 50;
+
+let qTotal = [1,0,0,0];
+const sq2 = Math.sqrt(2);
+let vPoints = [
+  [1,0,0],
+  [-1,0,0],
+  [1/sq2,1/sq2,0],
+  [-1/sq2,-1/sq2,0],
+  [1/sq2,-1/sq2,0],
+  [-1/sq2,1/sq2,0],
+  [0,1,0],
+  [0,-1,0],
+  [0,0,1],
+  [0,0,-1],
+];
+
+const qStepX = qMake(Math.PI/10,[1,0,0]);
+const qStepY = qMake(Math.PI/10,[0,1,0]);
+const qStepZ = qMake(Math.PI/10,[0,0,1]);
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function draw(rX, rY) {
+  ctx.beginPath();
+  ctx.arc(canvas.width/2+rX, canvas.height/2+rY,5,0,2*Math.PI);
+  ctx.fillStyle = "white";
+  ctx.fill();
+}
+
+function redraw() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  
+  ctx.beginPath();
+  ctx.arc(canvas.width/2, canvas.height/2,radius,0,2*Math.PI);
+  ctx.fillStyle = "blue";
+  ctx.fill();
+  
+  vPoints.forEach(vPoint=>{
+    const v = vRot(qTotal, vPoint);
+    if (v[2]>0) {
+      draw(v[0]*radius, v[1]*radius);
+    }
+  });
+}
+
+function reRun() {
+  resizeCanvas();
+  radius = Math.min(canvas.width, canvas.height)*0.3;
+  redraw();
+}
+
+function rotX() {
+  qTotal = qMult(qStepX, qTotal);
+  redraw();
+}
+
+function rotY() {
+  qTotal = qMult(qStepY, qTotal);
+  redraw();
+}
+
+function rotZ() {
+  qTotal = qMult(qStepZ, qTotal);
+  redraw();
+}
+
+reRun();
+
+window.addEventListener('resize', reRun);
