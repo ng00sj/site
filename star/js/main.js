61a284255d9d59a889acbfd09816c4aa90c98bd4
ng3small <hsuanyi03@gmail.com>
2026-08-15 09:34:50 +0800

    view

--- star/js/main.js
+++ star/js/main.js
@@ -89,7 +89,7 @@ fetch('data/under6-result_20260506_232500.csv').then((res)=>res.text()).then((cs
         pointsColor.push('rgb('+_r+','+_g+','+_b+')');
         pointsSize.push(0.2*Math.exp(-starData[i].phot_g_mean_mag));
       }
-      document.getElementById('text1').display = 'none';
+      document.getElementById('text1').style.display = 'none';
       reRun();
       window.addEventListener('resize', reRun);
     }
